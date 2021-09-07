b6358cc4c6d745ef76fcc953fbbd4c3cc2e9fee2
Chris Arnold <carnold84@users.noreply.github.com>
2021-09-08 10:03:20 +1200

    Feature/convert to pwa (#2)
    * Update router.js
    
    * Added pwa plugin

---
+++ docs/precache-manifest.fdc88c9f0fc1df1161a783c239b70290.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89d4f73b0952cd97dbe",
+    "url": "/tasks-app-prototype/css/app.4a34ae19.css"
+  },
+  {
+    "revision": "91348c5d762636d80264",
+    "url": "/tasks-app-prototype/css/chunk-vendors.e5419b3b.css"
+  },
+  {
+    "revision": "216f5d9978973e4425f6332b22d0cc06",
+    "url": "/tasks-app-prototype/index.html"
+  },
+  {
+    "revision": "e89d4f73b0952cd97dbe",
+    "url": "/tasks-app-prototype/js/app.a801e18d.js"
+  },
+  {
+    "revision": "91348c5d762636d80264",
+    "url": "/tasks-app-prototype/js/chunk-vendors.6b61d949.js"
+  },
+  {
+    "revision": "80d1240fa3f443cb4e487f11c9f78c7e",
+    "url": "/tasks-app-prototype/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tasks-app-prototype/robots.txt"
+  }
+]);
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   outputDir: 'docs',
+  pwa: {
+    name: 'Tasks',
+    themeColor: '#262a2c',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'GenerateSW',
+  },
   publicPath:
     process.env.NODE_ENV === 'production' ? '/tasks-app-prototype/' : '/',
 };
