c620850d12df94d17201c1b44d5f769092cb06f9
carnold84 <carnold84@users.noreply.github.com>
2021-09-12 15:56:35 +1200

    Updated service worker

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -9,12 +9,5 @@ self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
-});
-
-workbox.clientsClaim();
-
-// The precaching code provided by Workbox.
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+}); 
 
--- src/service-worker.js
+++ src/service-worker.js
@@ -7,11 +7,4 @@ self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
-});
-
-workbox.clientsClaim();
-
-// The precaching code provided by Workbox.
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+}); 
